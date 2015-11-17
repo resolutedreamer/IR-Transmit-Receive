6eb8edca9b6ce6a65fa7be835206c26f0ad6acea
Anthony Nguyen <asian701129@ucla.edu>
2015-11-17 01:48:38 -0800

    Removed Time-Multiplexing
    Changed IR Transmit code to constantly transmit on each channel.

--- pwm_transmit.c
+++ pwm_transmit.c
@@ -26,19 +26,32 @@
 #define PREAMBLE_DELAY 750000 // 10-11 miliseconds
 #define SHORT_DELAY 350000 // 5 miliseconds
 #define LONG_DELAY 1400000 // 20 miliseconds
+#define MID_DELAY 1050000 // 15 miliseconds
 
 
-void send_preamble_sequence(int preamble_duration, mraa_pwm_context pwm, float duty) {
+mraa_pwm_context pwm1;
+mraa_pwm_context pwm2;
+mraa_pwm_context pwm3;
+mraa_pwm_context pwm4;
+
+
+void send_preamble_sequence(int preamble_duration, float duty) {
 	int i = 0, j = 0;
 	printf("\nSending Preamble. ");
 
 	for(i = preamble_duration; i > 0; i--){
 		
 		//equal durations
-		mraa_pwm_write(pwm, duty); // high
+		mraa_pwm_write(pwm1, duty); // high
+		mraa_pwm_write(pwm2, duty); // high
+		mraa_pwm_write(pwm3, duty); // high
+		mraa_pwm_write(pwm4, duty); // high
 		for(j = PREAMBLE_DELAY; j > 0; j--);
 
-		mraa_pwm_write(pwm, 0); // low
+		mraa_pwm_write(pwm1, 0); // low
+		mraa_pwm_write(pwm2, 0); // low
+		mraa_pwm_write(pwm3, 0); // low
+		mraa_pwm_write(pwm4, 0); // low
 		for(j = PREAMBLE_DELAY; j > 0; j--);
 	}
 }
@@ -47,72 +60,34 @@ void send_low_bit(mraa_pwm_context pwm, float duty) {
 	int i = 0;
 	printf("0 ");
 	
-	mraa_pwm_write(pwm, duty);	// 5ms
-	for(i = SHORT_DELAY; i > 0; i--);
-	mraa_pwm_write(pwm, 0);		// 20ms
-	for(i = LONG_DELAY; i > 0; i--);	
+	mraa_pwm_write(pwm1, duty);
+	mraa_pwm_write(pwm2, duty);
+	mraa_pwm_write(pwm3, duty);
+	mraa_pwm_write(pwm4, duty);
+	for(i = SHORT_DELAY; i > 0; i--); // 5 ms
+	
+	mraa_pwm_write(pwm1, 0);
+	mraa_pwm_write(pwm2, 0);
+	mraa_pwm_write(pwm3, 0);
+	mraa_pwm_write(pwm4, 0);
+	for(i = LONG_DELAY; i > 0; i--); // 20 ms
 }
 
 void send_high_bit(mraa_pwm_context pwm, float duty) {
 	int i = 0;
 	printf("1 ");
 	
-	mraa_pwm_write(pwm, duty);	//20 ms
-	for(i = LONG_DELAY; i > 0; i--);
-	mraa_pwm_write(pwm, 0);		// 5ms
-	for(i = SHORT_DELAY; i > 0; i--);
-}
+	mraa_pwm_write(pwm1, duty);	
+	mraa_pwm_write(pwm2, duty);	
+	mraa_pwm_write(pwm3, duty);	
+	mraa_pwm_write(pwm4, duty);	
+	for(i = LONG_DELAY; i > 0; i--); //20 ms
 
-void send_message_bit(int edisonID, int irEmitterID, float duty, mraa_pwm_context pin) {
-	
-	// Preamble - Signals the Receiver Message Incoming
-	send_preamble_sequence(PREAMBLE_DURATION, pin, duty);
-	
-	// Sending Edison Board ID # - 2 bits, MSB then LSB
-	switch (edisonID){
-		case 0:
-			send_low_bit(pin, duty);	// Send lsb bit 0 = LOW
-			send_low_bit(pin, duty);	// Send msb bit 1 = LOW
-			break;
-		case 1:
-			send_high_bit(pin, duty);	// Send lsb bit 0 = HIGH
-			send_low_bit(pin, duty);	// Send msb bit 1 = LOW
-			break;
-		case 2:
-			send_low_bit(pin, duty);	// Send lsb bit 0 = LOW
-			send_high_bit(pin, duty);	// Send msb bit 1 = 
-			break;
-		case 3:
-			send_high_bit(pin, duty);	// Send lsb bit 0 = HIGH
-			send_high_bit(pin, duty);	// Send msb bit 1 = HIGH
-			break;
-		default:
-			send_low_bit(pin, duty);	// Send lsb bit 0 = LOW
-			send_low_bit(pin, duty);	// Send msb bit 1 = LOW				
-	}
-	
-	// Sending Edison IR Emitter ID # - 2 bits, MSB then LSB
-	switch (irEmitterID) {
-		case 0:
-			send_low_bit(pin, duty);	// Send lsb bit 0 = LOW
-			send_low_bit(pin, duty);	// Send msb bit 1 = 
-			break;
-		case 1:
-			send_high_bit(pin, duty);	// Send lsb bit 0 = HIGH
-			send_low_bit(pin, duty);	// Send msb bit 1 = LOW
-			break;
-		case 2:
-			send_low_bit(pin, duty);	// Send lsb bit 0 = LOW
-			send_high_bit(pin, duty);	// Send msb bit 1 = HIGH
-			break;
-		case 3:
-			send_high_bit(pin, duty);	// Send lsb bit 0 = HIGH
-			send_high_bit(pin, duty);	// Send msb bit 1 = HIGH
-			break;
-		default:
-			send_low_bit(pin, duty);	// Send lsb bit 0 = LOW
-			send_low_bit(pin, duty);	// Send msb bit 1 = LOW				
-	}
+	mraa_pwm_write(pwm1, 0);
+	mraa_pwm_write(pwm2, 0);
+	mraa_pwm_write(pwm3, 0);
+	mraa_pwm_write(pwm4, 0);
+	for(i = SHORT_DELAY; i > 0; i--); // 5 ms
 }
 
 int main(int argc, char *argv[]) {
@@ -121,22 +96,18 @@ int main(int argc, char *argv[]) {
 	// GPIO Initialization - Edison has 4 PWM pins
 	float duty = .5f;
 	
-	mraa_pwm_context pwm1;
-	pwm1 = mraa_pwm_init(5);
+	pwm1 = mraa_pwm_init(3);
 	mraa_pwm_period_us(pwm1, 26);
 	mraa_pwm_enable(pwm1, 1);
 	
-	mraa_pwm_context pwm2;
-	pwm2 = mraa_pwm_init(3);
+	pwm2 = mraa_pwm_init(5);
 	mraa_pwm_period_us(pwm2, 26);
 	mraa_pwm_enable(pwm2, 1);
 	
-	mraa_pwm_context pwm3;
 	pwm3 = mraa_pwm_init(6);
 	mraa_pwm_period_us(pwm3, 26);
 	mraa_pwm_enable(pwm3, 1);
 	
-	mraa_pwm_context pwm4;
 	pwm4 = mraa_pwm_init(9);
 	mraa_pwm_period_us(pwm4, 26);
 	mraa_pwm_enable(pwm4, 1);
@@ -150,8 +121,86 @@ int main(int argc, char *argv[]) {
 		if (argc == 2) {
 			edisonID = atoi(argv[1]);
 		}
-		// This will transmit IR data round-robin
+		// This will transmit IR data on all 4 pins at once
 		// on a single Edison board
+		
+		// Preamble - Signals the Receiver Message Incoming
+		send_preamble_sequence(PREAMBLE_DURATION, duty);
+		
+		// Sending Edison Board ID # - 2 bits, MSB then LSB
+		switch (edisonID){
+			case 0:
+				send_low_bit(duty);	// Send lsb bit 0 = LOW
+				send_low_bit(duty);	// Send msb bit 1 = LOW
+				break;
+			case 1:
+				send_high_bit(duty);	// Send lsb bit 0 = HIGH
+				send_low_bit(duty);	// Send msb bit 1 = LOW
+				break;
+			case 2:
+				send_low_bit(duty);	// Send lsb bit 0 = LOW
+				send_high_bit(duty);	// Send msb bit 1 = 
+				break;
+			case 3:
+				send_high_bit(duty);	// Send lsb bit 0 = HIGH
+				send_high_bit(duty);	// Send msb bit 1 = HIGH
+				break;
+			default:
+				send_low_bit(duty);	// Send lsb bit 0 = LOW
+				send_low_bit(duty);	// Send msb bit 1 = LOW				
+		}
+		
+		// Sending Edison IR Emitter ID # - 2 bits, MSB then LSB
+		
+		// pwm1 = 00 = short-long/short-long = 5-20/5-20
+		// pwm2 = 01 = short-long/long-short = 5-20/20-5
+		// pwm3 = 10 = long-short/short-long = 20-5/5-20
+		// pwm4 = 11 = long-short/long-short = 20-5/20-5
+		
+		// First Bit
+		mraa_pwm_write(pwm1, duty);
+		mraa_pwm_write(pwm2, duty);
+		mraa_pwm_write(pwm3, duty);
+		mraa_pwm_write(pwm4, duty);
+		for(i = SHORT_DELAY; i > 0; i--); // 5 ms
+		
+		mraa_pwm_write(pwm1, 0);
+		mraa_pwm_write(pwm2, 0);
+		mraa_pwm_write(pwm3, duty);
+		mraa_pwm_write(pwm4, duty);
+		for(i = MID_DELAY; i > 0; i--); // 15 ms
+
+		mraa_pwm_write(pwm1, 0);
+		mraa_pwm_write(pwm2, 0);
+		mraa_pwm_write(pwm3, 0);
+		mraa_pwm_write(pwm4, 0);
+		for(i = SHORT_DELAY; i > 0; i--); // 5 ms
+		
+		// Second Bit	
+		mraa_pwm_write(pwm1, duty);	
+		mraa_pwm_write(pwm2, duty);	
+		mraa_pwm_write(pwm3, duty);	
+		mraa_pwm_write(pwm4, duty);	
+		for(i = SHORT_DELAY; i > 0; i--); // 20 ms
+
+		mraa_pwm_write(pwm1, 0);	
+		mraa_pwm_write(pwm2, duty);	
+		mraa_pwm_write(pwm3, 0);	
+		mraa_pwm_write(pwm4, duty);	
+		for(i = MID_DELAY; i > 0; i--); // 15 ms
+
+		mraa_pwm_write(pwm1, 0);
+		mraa_pwm_write(pwm2, 0);
+		mraa_pwm_write(pwm3, 0);
+		mraa_pwm_write(pwm4, 0);
+		for(i = SHORT_DELAY; i > 0; i--); // 5 ms
+		
+		/*
+		send_message_bit(edisonID, 0, duty, pwm1);
+		send_message_bit(edisonID, 0, duty, pwm1);
+		send_message_bit(edisonID, 0, duty, pwm1);
+		send_message_bit(edisonID, 0, duty, pwm1);
+		
 		
 		for (transmit_counter = TRANSMIT_DURATION; transmit_counter >= 0; transmit_counter--) {
 			send_message_bit(edisonID, 0, duty, pwm1);
@@ -165,6 +214,7 @@ int main(int argc, char *argv[]) {
 		for (transmit_counter = TRANSMIT_DURATION; transmit_counter >= 0 ;transmit_counter--) {
 			send_message_bit(edisonID, 3, duty, pwm4);
 		}
+		*/
 		
 	}// end while loop
 
